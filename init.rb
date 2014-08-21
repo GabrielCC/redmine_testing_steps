34f6bb8e78d6e11d9218da6351a19d37f45e67fa
Gabriel Croitoru <gabriel.croitoru.cristian@gmail.com>
2014-08-21 14:44:58 +0300

    changes to the plugin

--- init.rb
+++ init.rb
@@ -1,37 +1,37 @@
 # Copyright (C) 2012-2013 Harry Garrood
-# This file is a part of redmine_release_notes.
+# This file is a part of redmine_testing_steps.
 
-# redmine_release_notes is free software: you can redistribute it and/or modify
+# redmine_testing_steps is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option) any
 # later version.
 
-# redmine_release_notes is distributed in the hope that it will be useful, but
+# redmine_testing_steps is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 # details.
 
 # You should have received a copy of the GNU General Public License along with
-# redmine_release_notes. If not, see <http://www.gnu.org/licenses/>.
+# redmine_testing_steps. If not, see <http://www.gnu.org/licenses/>.
 
 require 'redmine'
-require 'redmine_release_notes/hooks'
+require 'redmine_testing_steps/hooks'
 
 ActionDispatch::Callbacks.to_prepare do
   # Patches to the Redmine core.
   patched_classes = %w(issue issues_controller settings_controller version)
   patched_classes.each do |core_class|
     require_dependency core_class
-    "RedmineReleaseNotes::#{core_class.camelize}Patch".constantize.perform
+    "RedmineTestingSteps::#{core_class.camelize}Patch".constantize.perform
   end
 end
 
-Redmine::Plugin.register :redmine_release_notes do
-  name 'Redmine release notes plugin'
-  author 'Harry Garrood'
-  description 'A plugin for managing release notes.'
-  version '1.3.1'
-  author_url 'https://github.com/hdgarrood'
+Redmine::Plugin.register :redmine_testing_steps do
+  name 'Redmine testing steps plugin'
+  author 'Gabriel Croitoru'
+  description 'A plugin for managing testing steps.'
+  version '1.0.0'
+  author_url 'http://gabrielcc.github.io/'
   requires_redmine :version_or_higher => '2.1.0'
 
   # the partial won't be used, but can't be blank, because Redmine needs to
@@ -45,9 +45,9 @@ Redmine::Plugin.register :redmine_release_notes do
     },
     :partial => 'not_blank'
 
-  project_module :release_notes do
-    permission :release_notes,
-      { :release_notes => [:index, :new, :generate] },
+  project_module :redmine_testing_steps do
+    permission :redmine_testing_steps,
+      { :redmine_testing_steps => [:index, :new, :generate] },
       :public => true
   end
 
